1a415ab7186e708fc12685bb6e162ca3552e2ea1
Gabriel Silva <yzgabrieel02@gmail.com>
2024-09-03 17:53:18 -0300

    add new and update on `gitignores`

--- Interface/src/index.jsx
+++ Interface/src/index.jsx
@@ -5,8 +5,6 @@ import "./index.css";
 import Home from "pages/Home";
 import About from "pages/About";
 import Gallery from "pages/Gallery";
-import RegistrationForm from "components/RegistrationForm";
-import Budget from "pages/Budget";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -16,8 +14,6 @@ root.render(
         <Route exact path="/" element={<Home />} />
         <Route path="/sobre" element={<About />} />
         <Route path="/galeria" element={<Gallery />} />
-        <Route path="/registro" element={<RegistrationForm />} />
-        <Route path="/orcamento" element={<Budget />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
